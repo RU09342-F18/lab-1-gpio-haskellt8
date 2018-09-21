5d99700b0d8a3459da5d346857e8c9eabfaf4272
Unknown <haskellt8@students.rowan.edu>
2018-09-21 19:20:39 -0400

    update

--- Button Blink/MSP430F5529/Button Blink/main.c
+++ Button Blink/MSP430F5529/Button Blink/main.c
@@ -10,17 +10,17 @@ void main(void)
     WDTCTL = WDTPW + WDTHOLD; // stop watchdog timer
 
     P1DIR |= (BIT0); // set P1 Bit 0 to out, LED P1.0
-    P1DIR &= ~(BIT3); // set P1 Bit 3 to in
-    P1REN |= (BIT3); // enable P1 Bit 3 resistor
-    P1OUT |= (BIT3); // set P1 Bit 3 resistor to pull up
-    P1DIR |= (BIT6); // set P1 Bit 6 to out, LED P1.6
+    P2DIR &= ~(BIT1); // set P2 Bit 1 to in
+    P2REN |= (BIT1); // enable P1 Bit 1 resistor
+    P2OUT |= (BIT1); // set P2 Bit 1 resistor to pull up
+    P4DIR |= (BIT7); // set P1 Bit 6 to out, LED P4.7
 
     while (1) // always loop
     {
-        int button = P1IN & BIT3; // & P1.3 button with BIT3 to get button status
+        int button = P2IN & BIT1; // & P1.3 button with BIT3 to get button status
         if (button == 0) // button is 0 when P1.3 button is pressed
         {
-            P1OUT &= ~BIT6; // force LED P1.6 off
+            P4OUT &= ~BIT7; // force LED P1.6 off
             long j; // declared outside of for loop to compile
             for( j = 10*TENTHS; j > 0; j-- ) // loop for a tenth of a second
             {
@@ -38,7 +38,7 @@ void main(void)
             {
                 if (j == 1) // first iteration of loop
                 {
-                    P1OUT ^= BIT6; // flip LED
+                    P4OUT ^= BIT7; // flip LED
                 }
             }
         }
